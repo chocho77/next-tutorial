aeb1d3a135c1209154c88c0e23be7c5313d79679
chavdar <chavmom@gmail.com>
2022-12-12 08:52:58 +0200

    Changes to be committed: 	modified:   pages/index.tsx 	modified:   pages/profile/[username].js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      {" "}
+      <Link href="/about">About</Link>
       <h1>Hello World</h1>
      
     </div>
-  )
+  );
 }
