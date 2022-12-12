10616d9cbf778be02313fcdebff8270cb54e3b7f
chavdar <chavmom@gmail.com>
2022-12-12 08:59:22 +0200

    Changes to be committed: 	modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import Head from "next/head";
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>ChochoTech Next Tutorial</title>
+      </Head>
       <Link href="/about">About</Link>
       <h1>Hello World</h1>
      
